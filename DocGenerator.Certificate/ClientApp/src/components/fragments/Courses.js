76d98c6beb62c89f0fc6568981bb1221889b2e47
Thiago Yuri <thiagoyuri0109@gmail.com>
2022-03-30 14:00:04 -0300

    DocGenerator.Certificate - Create components

---
+++ DocGenerator.Certificate/ClientApp/src/components/fragments/Courses.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Courses extends Component {
+  render () {
+    return (
+        <div>
+
+      </div>
+    );
+  }
+}
---
+++ DocGenerator.Certificate/ClientApp/src/components/layout/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+
+  render () {
+    return (
+      <div>
+      
+      </div>
+    );
+  }
+}
