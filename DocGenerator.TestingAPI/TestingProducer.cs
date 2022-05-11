baa2cf23d1485d6f6849f2fb2b29fdd850bdebec
Thiago Yuri <thiagoyuri0109@gmail.com>
2022-05-10 21:29:10 -0300

    Document Generator API: Producer project test

---
+++ DocGenerator.TestingAPI/TestingProducer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DocGenerator.TestingAPI
+{
+    
+    public class TestingProducer
+    {
+        [Fact]
+        public void TestMethodPost()
+        {
+
+        }
+
+        [Fact]
+        public void TestMethodGet()
+        {
+
+        }
+    }
+}
